c6622573fee03343ed3413af75b859b26f904682
Shiftypanda <drtimddavis@gmail.com>
2018-08-10 22:49:03 +0100

    updated camodo and squaishey

--- camodo_gaming.py
+++ camodo_gaming.py
@@ -4,3 +4,10 @@
 https://www.youtube.com/playlist?list=PLN_hC9GPyucy8yGaIx77x32jWnc-yuf8z
 
 youtube-dl --no-check-certificate -f 18 -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --playlist-start 1 https://www.youtube.com/playlist?list=PLN_hC9GPyucy8yGaIx77x32jWnc-yuf8z
+
+
+# Camodo gaming - brick rigs - biiig playlist
+
+https://www.youtube.com/watch?v=1H1FrCwPQM8&list=PLN_hC9GPyuczO-UBOAbbgaNTvdKe8t8zw
+
+youtube-dl --no-check-certificate -f 18 -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --playlist-start 1 https://www.youtube.com/playlist?list=PLN_hC9GPyuczO-UBOAbbgaNTvdKe8t8zw
---
+++ sqaishey_quack.py
@@ -0,0 +1,2 @@
+# sqaishey quack - netty's house playlist
+youtube-dl --no-check-certificate -f 18 -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLa3XidtLkKrd1NPhm9x_euHQlMIRa_YmF
