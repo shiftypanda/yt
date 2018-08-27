abb1b2acd35336fefa8fb21c92a78727a58fce85
Shiftypanda <drtimddavis@gmail.com>
2018-08-27 11:19:28 +0100

    amended to deactivate virtualenv and exit terminal

--- raspbian_script.sh
+++ raspbian_script.sh
@@ -1,9 +1,14 @@
 # shell script to search and update
-source /home/pi/coding/videoscrapers/youtube-dl/virtualenv/bin/activate
+# mount all network drievs
 sudo mount -a
+
+# activate python 3.7 virtual env
+source /home/pi/coding/videoscrapers/youtube-dl/virtualenv/bin/activate
+
+# change directory to shared videos for youtubers
 cd /home/pi/videos/Public/Shared\ Videos/Youtubers
 
-# Download good general quality -- DEFAULT
+# example for Download good general quality -- DEFAULT
 # youtube-dl --no-check-certificate -f 18 -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=RESTOFURLHERE
 
 # Camodo gaming - brick rigs - biiig playlist
@@ -11,3 +16,9 @@ youtube-dl --no-check-certificate -f 18 -o 'camodo\ gaming/%(playlist)s/%(playli
 
 # popular mmos minecraft mods
 youtube-dl --no-check-certificate -f 18 -o 'popularmmos/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PL6p1NYDZ87wKTp2iIS0s7BUobisFVzrmW
+
+# deactivate virtualenv
+deactivate
+
+# exit terminal
+exit
