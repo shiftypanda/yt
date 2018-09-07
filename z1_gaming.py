8f1a6ef12e5c9a6a934beba038ecce3736a0e2af
Shiftypanda <drtimddavis@gmail.com>
2018-09-07 16:35:37 +0100

    updated new z1gaming astoneer

--- z1_gaming.py
+++ z1_gaming.py
@@ -9,6 +9,11 @@ https://www.youtube.com/playlist?list=PL8Hr8BU6bPOlJp_37y0-CCCpsumTKgYLC
 Season 1
 https://www.youtube.com/playlist?list=PL8Hr8BU6bPOkwViTPGK6CMqz-irUmo0cF
 
+astroneer 10
+https://www.youtube.com/watch?v=MA3yDY_r2CA&list=PL8Hr8BU6bPOm_dNQCxgZuL4sBAnYI44se
+
+astroneer 9.2
+https://www.youtube.com/watch?v=N5vb4eW4NKA&list=PL8Hr8BU6bPOmjRZtqUGGdp1kSODPf63Dy
 
 Astroneer 9.0
 https://www.youtube.com/playlist?list=PL8Hr8BU6bPOm0_Uhihi9GRTmtBt-npEk3
