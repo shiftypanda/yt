64063b87e4568d19a3cc4118ff588d78a691abae
Tim Davis <drtimddavis@gmail.com>
2019-02-19 20:14:32 +0000

    added frantic matt

---
+++ frantic_matt.py
@@ -0,0 +1,6 @@
+# frantic matt downloads
+
+# stormworks playlist
+https://www.youtube.com/watch?v=hZ1QF46rxE8&list=PLaeOtLvtDiQl1BwPMGRAD6sdaSi0jUYsF
+
+youtube-dl --no-check-certificate -f 18 -o '/home/pi/videos/Shared Videos/Youtubers/frantic_matt/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLaeOtLvtDiQl1BwPMGRAD6sdaSi0jUYsF
