4514faa0a12e723e764e6cd8a5dbdafb27acaf0c
Shiftypanda <drtimddavis@gmail.com>
2020-03-16 08:38:55 +0000

    added school of dragons to little lizard links

--- little_lizard.py
+++ little_lizard.py
@@ -1,3 +1,8 @@
 # little lizard how to train your dragon
 https://www.youtube.com/playlist?list=PLE_YD57tF8CUSyWYOBt1rMIwWli5IfdAE
 youtube-dl --no-check-certificate -f 18 -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' --playlist-start 1 https://www.youtube.com/playlist?list=PLE_YD57tF8CUSyWYOBt1rMIwWli5IfdAE
+
+
+# little lizard school of dragons
+youtube-dl --no-check-certificate -f 18 -o 'little_lizard/school_of_dragons/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLXVyYfviy2ilKkpwZyjbe3Qxwr7LTxVdW
+
