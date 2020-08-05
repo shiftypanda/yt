48ce2263917d99426f5b99f36726004f85f4b33b
Tim Davis <drtimddavis@gmail.com>
2020-08-05 11:28:00 +0100

    updated playlist starts for weekly download check

--- weekly_download_check.sh
+++ weekly_download_check.sh
@@ -13,7 +13,7 @@ cd /home/pi/videos/YouTubers
 # youtube-dl --no-check-certificate -f 18 --playlist-start 574 -o 'camodo gaming/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLN_hC9GPyuczO-UBOAbbgaNTvdKe8t8zw
 
 # camodo gaming - stormworks
-youtube-dl --no-check-certificate -f 18 --playlist-start 421 -o 'camodo gaming/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLN_hC9GPyucy8yGaIx77x32jWnc-yuf8z
+youtube-dl --no-check-certificate -f 18 --playlist-start 434 -o 'camodo gaming/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLN_hC9GPyucy8yGaIx77x32jWnc-yuf8z
 
 
 
@@ -33,31 +33,40 @@ youtube-dl --no-check-certificate -f 18 --playlist-start 421 -o 'camodo gaming/%
 # scrap man - scrap mechanics
 # youtube-dl --no-check-certificate -f 18 --playlist-items 1-10 -o 'scrapman/scrap_mechanics/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s'  https://www.youtube.com/playlist?list=PLjtiR7dM6Lgo2oINNGBVQ8UMT6k3dM9df
 
+
+
 # scrap man - scrap mechanics survival
-youtube-dl --no-check-certificate -f 18 --playlist-start 14 -o 'scrapman/scrap_mechanics/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLjtiR7dM6LgpI9BkWeWsnhxT070_Y92QQ
+youtube-dl --no-check-certificate -f 18 --playlist-start 54 -o 'scrapman/scrap_mechanics/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLjtiR7dM6LgpI9BkWeWsnhxT070_Y92QQ
+
+# kan gaming - scrap mechanics survival
+youtube-dl --no-check-certificate -f 18 --playlist-start 30 -o 'kan_gaming/scrap_mechanics/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLAYl0rkyP6d7bW34hwgtvmXfMMYrlQyqq
+
+
 
 # iballistic squid - scrap mechanics survival
-youtube-dl --no-check-certificate -f 18 --playlist-start 18 -o 'iballisticsquid/scrap mechanic/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PL0hoUWUYryn6lZl1BilQaL29E3L5cfXsK
+youtube-dl --no-check-certificate -f 18 --playlist-start 33 -o 'iballisticsquid/scrap mechanic/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PL0hoUWUYryn6lZl1BilQaL29E3L5cfXsK
 
 # stampy - animal crossing live streams
-youtube-dl --no-check-certificate -f 18 --playlist-start 17 -o 'stampy_cat/animal_crossing/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PL0hoUWUYryn5bmEarMzicBvEk-8BaiXk8
+youtube-dl --no-check-certificate -f 18 --playlist-start 23 -o 'stampy_cat/animal_crossing/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PL0hoUWUYryn5bmEarMzicBvEk-8BaiXk8
 
 
 # camodo gaming - scrap mechanics
 
-youtube-dl --no-check-certificate -f 18 --playlist-start 1 -o 'camodo gaming/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLN_hC9GPyucyMzKEGFKwjJcyA9wlWVvRW
+youtube-dl --no-check-certificate -f 18 --playlist-start 22 -o 'camodo gaming/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLN_hC9GPyucyMzKEGFKwjJcyA9wlWVvRW
 
 # stampy - spaceden
-youtube-dl --no-check-certificate -f 18 --playlist-start 28 -o 'stampy_cat/minecraft/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLEZiAg2bYC7kxNg5rwfcH8RQbsBkBxi__
+youtube-dl --no-check-certificate -f 18 --playlist-start 43 -o 'stampy_cat/minecraft/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLEZiAg2bYC7kxNg5rwfcH8RQbsBkBxi__
 
 # stampy minecraft dungeons
-youtube-dl --no-check-certificate -f 18 --playlist-start 3 -o 'stampy_cat/minecraft_dungeons/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PL0hoUWUYryn4Pu5woI3dH7KQmVL6v_-NZ 
+# youtube-dl --no-check-certificate -f 18 --playlist-start 4 -o 'stampy_cat/minecraft_dungeons/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PL0hoUWUYryn4Pu5woI3dH7KQmVL6v_-NZ 
 
-# scrap man - stormworks
+# beautiful ob - totally reliable
+# youtube-dl --no-check-certificate -f 18 --playlist-start 4 -o 'camodo_gaming/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PL0hoUWUYryn5W9f8kqLX87agBnM1gWseH
+
 # youtube-dl --no-check-certificate -f 18 --playlist-start 1 -o 'scrapman/stormworks/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLjtiR7dM6LgpTxR6InOOL6zrrbpz91FMm
 
 # frantic matt - stormworks
-# youtube-dl --no-check-certificate -f 18 --playlist-items 1-20 -o 'frantic_matt/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLaeOtLvtDiQl1BwPMGRAD6sdaSi0jUYsF
+youtube-dl --no-check-certificate -f 18 --playlist-items 1-20 -o 'frantic_matt/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLaeOtLvtDiQl1BwPMGRAD6sdaSi0jUYsF
 
 # kan gaming - scrap mechanic
 # youtube-dl --no-check-certificate -f 18 --playlist-start 470 -o 'kan_gaming/scrap_mechanic/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLAYl0rkyP6d6H802FAldqw5BbDQarsqRf
@@ -72,5 +81,7 @@ youtube-dl --no-check-certificate -f 18 --playlist-start 3 -o 'stampy_cat/minecr
 # youtube-dl --no-check-certificate -f 18 --playlist-start 1 -o 'kan_gaming/stormworks/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLAYl0rkyP6d5pek8iPQGonyjddNcg5OLS
 
 # mumbo jumbo - hermitcraft
-# youtube-dl --no-check-certificate -f 18 --playlist-start 128  -o 'mumbo_jumbo/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s'  https://www.youtube.com/playlist?list=PLFm1tTY1NA4fcZddqsijPEignAza8nF74
+# youtube-dl --no-check-certificate -f 18 --playlist-start 134 -o 'mumbo_jumbo/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLFm1tTY1NA4fcZddqsijPEignAza8nF74
 
+# mumbo jumbo - hermitcraft season 7
+youtube-dl --no-check-certificate -f 18 --playlist-start 39 -o 'mumbo_jumbo/%(playlist)s/%(upload_date)s - %(title)s.%(ext)s' https://www.youtube.com/playlist?list=PLFm1tTY1NA4eFO89sYmMDVghvH0m2wUmc
